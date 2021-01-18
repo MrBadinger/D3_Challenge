e441d3b32cd91f70a842cf9d953068d0a414b5eb
Michael Badinger <mcbadinger@gmail.com>
2021-01-17 19:15:17 -0600

    set up step 1 data parse

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -13,10 +13,22 @@ var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3.select(".chart")
+var svg = d3.select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
 
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Import Data
+d3.csv("assets/data/data.csv").then(function(CSV_Data) {
+
+    // Step 1: Parse Data/Cast as numbers
+    // ==============================
+    CSV_Data.forEach(function(data) {
+      data.poverty  = +data.poverty;
+      data.healthcare = +data.healthcare;
+    });
+
+   
